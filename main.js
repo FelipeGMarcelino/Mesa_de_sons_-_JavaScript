820f381a1461085b3a026432535082f984bc299c
Extintor <felipe.gabriel.stz@hotmail.com>
2023-05-29 11:28:51 -0300

    changed file names, added "telefone" functionality

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     
     const instrumento = listaDeTeclas[contador].classList[1];
@@ -16,6 +14,4 @@ while (contador < listaDeTeclas.length) {
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-
-    contador++;
 }
